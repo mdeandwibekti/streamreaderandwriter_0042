12b228ba6ab553287e97d09f0b4762e61f83a9c2
mdeandwibekti <mdeandwibekti@gmail.com>
2024-06-04 18:21:19 +0700

    int main try

---
+++ exeptionstandard/exeptionstandard.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <exception>
+#include <array>
+using namespace std;
+
+int main()
+{
+	cout << "Awal Program" << endl; //penanda 1:...
+	try {
+		array<int, 3> data = { 3, 5 ,7 };
+		//pesan array integer 3 elemen
+		cout << data.at(5) << endl;
+		//memanggil array elemen ke 5
+	}
+	
