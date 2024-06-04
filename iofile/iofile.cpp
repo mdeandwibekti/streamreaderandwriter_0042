5eec0a0285e9b906fe7ce7d47464c3cd69bb6e29
mdeandwibekti <mdeandwibekti@gmail.com>
2024-06-04 17:28:16 +0700

    while,memasukan cout if

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -3,5 +3,27 @@
 #include <string>
 using namespace std;
 
+int main() {
+	string baris;
+
+	ofstream outfile;
+	outfile.open("cintihfile.txt");
+
+	cout << ">=menulis file, \'q\' untuk keluar" << endl;
+
+	while (true) {
+		cout << "- ";
+		getline(cin, baris);
+		if (baris == "q") break;
+		outfile << baris << endl;
+	}
+
+	outfile.close();
+	ifstream infile;
+	infile.open("contohfile.txt");
+
+	cout << endl << ">= membuka dan membaca file " << endl;
+
+
 
 }
