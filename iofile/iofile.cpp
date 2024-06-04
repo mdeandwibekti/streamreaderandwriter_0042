758ff8dc8c6550caa4b4087fa9781a3f8f26f2f0
mdeandwibekti <mdeandwibekti@gmail.com>
2024-06-04 17:25:03 +0700

    variable global

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -3,13 +3,5 @@
 #include <string>
 using namespace std;
 
-int main() {
-	string baris;
-
-	ofstream outfile;
-	outfile.open("cintihfile.txt");
-
-	cout << ">=menulis file, \'q\' untuk keluar" << endl;
-
 
 }
